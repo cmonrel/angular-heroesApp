e7b9537d07203f43edd007593a6e6a5ca124b9f8
Carlos Monserrat Relaño <carlosmr00@gmail.com>
2024-04-12 14:02:37 +0200

    Primera actualización del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -10,10 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+@Component({
+  selector: 'heroes-search-page',
+  templateUrl: './search-page.component.html',
+  styles: ``
+})
+export class SearchPageComponent {
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero ?: Hero;
+
+  constructor(private heroesService: HeroesService) {}
+
+  public searchHero() {
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService.getSuggestions(value)
+      .subscribe(heroes => this.heroes = heroes)
+  }
+
+  public onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option.value){
+      this.selectedHero = undefined;
+      return;
+    }
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+
+    this.selectedHero = hero;
+  }
+}
